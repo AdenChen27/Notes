8857a67c4ddef7c447d5edad5c9e6685651f8f99
AdenChen27 <AdenChen2005@gmail.com>
2024-10-26 16:09:25 -0500

    notes

--- pull-notes.sh
+++ pull-notes.sh
@@ -1,3 +1,3 @@
-cp /Users/aden/Library/CloudStorage/Box-Box/Courses/ECON200/notes.pdf "ECON 20010 - The Elements of Economic Analysis I Honors.pdf"
-cp /Users/aden/Library/CloudStorage/Box-Box/Courses/STAT244/notes.pdf "STAT 24410 - Statistical Theory and Methods Ia.pdf"
+cp /Users/aden/Courses/ECON200/notes.pdf "ECON 20010 - The Elements of Economic Analysis I Honors.pdf"
+cp /Users/aden/Courses/STAT244/notes.pdf "STAT 24410 - Statistical Theory and Methods Ia.pdf"
 
