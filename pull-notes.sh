faa408b8ec8479f59c916be9087d6acddf2e0cf5
AdenChen27 <AdenChen2005@gmail.com>
2024-10-31 22:19:35 -0500

    updated notes

--- pull-notes.sh
+++ pull-notes.sh
@@ -1,3 +1,9 @@
+# pull notes
 cp /Users/aden/Courses/ECON200/notes.pdf "ECON 20010 - The Elements of Economic Analysis I Honors.pdf"
 cp /Users/aden/Courses/STAT244/notes.pdf "STAT 24410 - Statistical Theory and Methods Ia.pdf"
+
+
+git add -u
+git commit -m "updated notes"
+got push
 
