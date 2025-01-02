87eab9d42b052d24e407cb547d72d477320b304b
AdenChen27 <AdenChen2005@gmail.com>
2025-01-02 13:48:52 +0800

    updated notes

--- update-notes.sh
+++ update-notes.sh
@@ -1,8 +1,8 @@
 # pull notes
 cd /Users/aden/Box/PublicNotes/
 # 2024 Fall
-# cp /Users/aden/Courses/ECON200/notes.pdf "ECON 20010 - The Elements of Economic Analysis I Honors.pdf"
-# cp /Users/aden/Courses/STAT244/notes.pdf "STAT 24410 - Statistical Theory and Methods Ia.pdf"
+cp /Users/aden/Courses/ECON200/notes.pdf "ECON 20010 - The Elements of Economic Analysis I Honors.pdf"
+cp /Users/aden/Courses/STAT244/notes.pdf "STAT 24410 - Statistical Theory and Methods Ia.pdf"
 
 # 2025 Winter
 cp /Users/aden/Courses/ECON201/notes.pdf "ECON 20110 - The Elements of Economic Analysis II Honors.pdf"
