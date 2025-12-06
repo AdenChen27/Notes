9e4eaf1a4c0421e0971f37d10df404897f6e35c6
AdenChen27 <adenchen2005@gmail.com>
2025-12-06 12:40:15 -0600

    updated README

--- update-notes.sh
+++ update-notes.sh
@@ -1,23 +1,23 @@
 # pull notes
 cd /Users/aden/Box/PublicNotes/
 # 2024 Fall
-cp /Users/aden/Courses/ECON200/notes.pdf "ECON 20010 - The Elements of Economic Analysis I Honors.pdf"
-cp /Users/aden/Courses/STAT244/notes.pdf "STAT 24410 - Statistical Theory and Methods Ia.pdf"
-cp /Users/aden/Courses/STAT244/midterm-cheatsheet.pdf "STAT24410-midterm-cheatsheet.pdf"
-cp /Users/aden/Courses/STAT244/cheatsheet.pdf "STAT24410-cheatsheet.pdf"
-
-# 2025 Winter
-cp /Users/aden/Courses/ECON201/notes.pdf "ECON 20110 - The Elements of Economic Analysis II Honors.pdf"
-cp /Users/aden/Courses/ECON201/cheatsheet.pdf "ECON20110-cheatsheet.pdf"
-cp /Users/aden/Courses/MATH204/notes.pdf "MATH 20410 - Analysis in Rn II (accelerated).pdf"
-cp /Users/aden/Courses/STAT245/notes.pdf "STAT 24510 - Statistical Theory and Methods IIa.pdf"
-cp /Users/aden/Courses/STAT245/midterm-cheatsheet.pdf "STAT24510-midterm-cheatsheet.pdf"
-cp /Users/aden/Courses/STAT245/cheatsheet.pdf "STAT24510-cheatsheet.pdf"
-
-# 2025 Spring
-cp /Users/aden/Courses/ECON21030/notes.pdf "ECON 21030 - Honors Econometrics I.pdf"
-cp /Users/aden/Courses/ECON202/notes.pdf "ECON 20210 - The Elements of Economic Analysis III Honors.pdf"
-cp /Users/aden/Courses/MATH205/notes.pdf "MATH 20510 - Analysis in Rn III (accelerated).pdf"
+# cp /Users/aden/Courses/ECON200/notes.pdf "ECON 20010 - The Elements of Economic Analysis I Honors.pdf"
+# cp /Users/aden/Courses/STAT244/notes.pdf "STAT 24410 - Statistical Theory and Methods Ia.pdf"
+# cp /Users/aden/Courses/STAT244/midterm-cheatsheet.pdf "STAT24410-midterm-cheatsheet.pdf"
+# cp /Users/aden/Courses/STAT244/cheatsheet.pdf "STAT24410-cheatsheet.pdf"
+#
+# # 2025 Winter
+# cp /Users/aden/Courses/ECON201/notes.pdf "ECON 20110 - The Elements of Economic Analysis II Honors.pdf"
+# cp /Users/aden/Courses/ECON201/cheatsheet.pdf "ECON20110-cheatsheet.pdf"
+# cp /Users/aden/Courses/MATH204/notes.pdf "MATH 20410 - Analysis in Rn II (accelerated).pdf"
+# cp /Users/aden/Courses/STAT245/notes.pdf "STAT 24510 - Statistical Theory and Methods IIa.pdf"
+# cp /Users/aden/Courses/STAT245/midterm-cheatsheet.pdf "STAT24510-midterm-cheatsheet.pdf"
+# cp /Users/aden/Courses/STAT245/cheatsheet.pdf "STAT24510-cheatsheet.pdf"
+#
+# # 2025 Spring
+# cp /Users/aden/Courses/ECON21030/notes.pdf "ECON 21030 - Honors Econometrics I.pdf"
+# cp /Users/aden/Courses/ECON202/notes.pdf "ECON 20210 - The Elements of Economic Analysis III Honors.pdf"
+# cp /Users/aden/Courses/MATH205/notes.pdf "MATH 20510 - Analysis in Rn III (accelerated).pdf"
 
 # 2025 Fall
 cp /Users/aden/Courses/MATH235/notes.pdf "MATH 23500 - Markov Chains, Martingales, and Brownian Motion.pdf"
